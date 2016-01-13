3f4fad45057d1a40e920c21df38550bd37642fe6
Sarah Hersh <schersh@gmail.com>
2016-01-13 09:00:29 -0500

    added styles to main page

--- js/ingredients/index.controller.js
+++ js/ingredients/index.controller.js
@@ -2,19 +2,19 @@
 
 (function(){
   angular
-  .module("bibimbap")
-  .controller("IngredientsIndexController", [
-    "IngredientsIndexControllerFunction"
+  .module("ingredients")
+  .controller("IngredientIndexController", [
+    "ControllerFunction"
   ]);
 
-  function IngredientsIndexControllerFunction(){
+  function ControllerFunction(){
     this.grain = [
       "Wild Rice",
       "Basmati Rice",
       "Quinoa",
       "Couscous",
       "Bulgur"
-    ],
+    ];
     this.protein = [
       "Fried Egg",
       "Tofu",
@@ -22,20 +22,32 @@
       "Minced Beef",
       "Roasted Cashews",
       "Roasted Chickpeas"
-    ],
+    ];
     this.veg = [
       "Shiitake Mushrooms",
       "Shredded Carrots",
       "Seasoned Spinach",
       "Bean Sprouts",
       "Radishes"
-    ],
+    ];
     this.sauce = [
       "Sesame oil, sugar, apple cider vinegar, minced garlic, gochujang (Korean chili paste)",
       "Brown sugar, soy sauce, ginger",
       "Lemon juice, olive oil, soy sauce, sesame seeds",
       "Rice vinegar, soy sauce, olive oil",
       "Honey, dijon mustard, olive oil, red pepper flakes"
-    ]
+    ];
+    this.showRecipe = function(){
+      // toggle class to show div on index.html which will display ul li of recipe obj
+    };
+    this.spinBowl = function(){
+      console.log('spinning bowl')
+      // toggle class to activate css transitions
+      // set timeout
+      // remove class
+      // create new recipe obj with randomly selected ingredients
+      // call show recipe function(recipe obj)
+    };
+
   }
 })();
